f7f648af06acbdf5160bb45e33f49003e8e0f024
Bhushan-Dudhal <bhushandhudhal9@gmail.com>
2024-04-21 19:44:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+      function incre() {
+        var n = document.getElementById("abc").value;
+        n++;
+        document.getElementById("abc").value = n;
+      }
+
+      function decre() {
+        var n = document.getElementById("abc").value;
+        n--;
+        document.getElementById("abc").value = n;
+      }
+    
